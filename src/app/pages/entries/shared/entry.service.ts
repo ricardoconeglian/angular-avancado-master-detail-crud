5d42402baf9642effc8ed6f1fe00c1fe7c7fdc82
ricardoconeglian <72042399+ricardoconeglian@users.noreply.github.com>
2021-11-23 15:28:05 -0300

    improving EntryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,7 +3,7 @@ import { BaseResourceService } from 'src/app/shared/services/base-resource.servi
 import {Entry} from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
 import {Observable} from 'rxjs';
-import {mergeMap} from 'rxjs/operators';
+import {mergeMap, catchError} from 'rxjs/operators';
 
 
 
@@ -20,24 +20,22 @@ export class EntryService extends BaseResourceService<Entry> {
 
 
   create(entry: Entry): Observable<Entry> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      mergeMap(category => {
-        entry.category = category;
-        return super.create(entry);
-      })
-    )
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this))
   }
 
   update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this))
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
     return this.categoryService.getById(entry.categoryId).pipe(
       mergeMap(category => {
         entry.category = category;
-        return super.update(entry)
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     )
 
-
   }
-
 
 }
